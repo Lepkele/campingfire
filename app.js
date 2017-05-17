9f196bc0c088263aa6a9c1169f2ef73798f78bc9
Lepkele <charlessport2000@yahoo.com>
2017-05-17 08:06:36 +0000

    update dburl

--- app.js
+++ app.js
@@ -20,9 +20,11 @@ var commentRoutes    = require("./routes/comments"),
 app.locals.moment = require('moment');
 
 // connect to  database
-// var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v10";
+var url = process.env.DATABASEURL;
 
- var url = process.env.DATABASEURL || "mongodb://charles:london@ds143081.mlab.com:43081/campfire";
+// var url = process.env.DATABASEURL || "mongodb://charles:london@ds143081.mlab.com:43081/campfire";
+
+// console.log(process.env.DATABASEURL);
 
 mongoose.connect(url);
 
@@ -63,6 +65,10 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(process.env.PORT || 3000, process.env.IP, function(){
+// app.listen(process.env.PORT || 3000, process.env.IP, function(){
+//   console.log("The YelpCamp Server Has Started!");
+// });
+
+app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
 });
